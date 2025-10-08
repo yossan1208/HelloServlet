82228b138ef3b01844ef595765fcc2ba8340b479
yossan1208 <yossan431208@gmail.com>
2025-10-08 22:36:37 +0900

    feat: WelcomeServlet.javaを作成する

---
+++ src/main/java/WelcomeServlet.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		String username = request.getParameter("username");
+		System.out.println(username);
+		
+		PrintWriter out=response.getWriter();
+		out.println("<html><head></head><body><br>" + username + "さん、ようこそ！</body></html>");
+//		doGet(request, response);
+	}
+
+}
